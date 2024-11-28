9dd8d92f016437cdba563f7bdafc0d5b5c6fc3ab
aditya <adityasahai205@gmail.com>
2024-11-28 14:36:51 +0530

    added visualization of graph updation

--- Wifi_Simulator/main.cpp
+++ Wifi_Simulator/main.cpp
@@ -1,14 +1,86 @@
 #include <iostream>
+#include <fstream>
 #include <memory>
 #include "wifi_simulator.h"
 
-int main() {
-    int users[] = {1, 10, 100};
-    for (int numUsers : users) {
-        std::cout << "\nSimulating with " << numUsers << " user(s):\n";
+class TeeStream {
+    std::ostream& console;
+    std::ofstream& logFile;
+public:
+    TeeStream(std::ostream& console, std::ofstream& logFile) 
+        : console(console), logFile(logFile) {}
 
-        WiFiSimulator simulator(numUsers);
-        simulator.runSimulation();
+    template<typename T>
+    TeeStream& operator<<(const T& data) {
+        console << data;
+        logFile << data;
+        return *this;
     }
+
+    // Handle endl and other manipulators
+    TeeStream& operator<<(std::ostream& (*manip)(std::ostream&)) {
+        console << manip;
+        logFile << manip;
+        return *this;
+    }
+};
+
+void displayMenu(TeeStream& output) {
+    output << "Select the number of users to simulate:\n";
+    output << "1. 1 user\n";
+    output << "2. 10 users\n";
+    output << "3. 100 users\n";
+    output << "4. Simulate all options\n";
+    output << "Enter your choice: ";
+}
+
+int main() {
+    std::ofstream logFile("simulation_log.txt", std::ios::app);
+    TeeStream output(std::cout, logFile);
+
+    while (true) {
+        displayMenu(output);
+
+        int choice;
+        std::cin >> choice;
+
+        if (choice < 1 || choice > 4) {
+            output << "Invalid choice. Please try again.\n";
+            continue;
+        }
+
+        std::vector<int> users;
+
+        switch (choice) {
+            case 1:
+                users.push_back(1);
+                break;
+            case 2:
+                users.push_back(10);
+                break;
+            case 3:
+                users.push_back(100);
+                break;
+            case 4:
+                users = {1, 10, 100};
+                break;
+        }
+
+        for (int numUsers : users) {
+            output << "\nSimulating with " << numUsers << " user(s):\n";
+            WiFiSimulator simulator(numUsers);
+            simulator.runSimulation();
+        }
+
+        // Ask if user wants to run another simulation
+        char runAgain;
+        output << "Would you like to run another simulation? (y/n): ";
+        std::cin >> runAgain;
+
+        if (runAgain == 'n' || runAgain == 'N') {
+            break;
+        }
+    }
+
     return 0;
 }
