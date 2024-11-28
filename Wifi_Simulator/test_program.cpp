bdeb55453f4a3606d6f72f1573af76a9aa51a7fd
aditya <adityasahai205@gmail.com>
2024-11-29 01:17:26 +0530

    updated number of users

--- Wifi_Simulator/test_program.cpp
+++ Wifi_Simulator/test_program.cpp
@@ -2,15 +2,9 @@
 #include <iostream>
 
 int main() {
-    // Number of users for the simulation
 
-    for(int i=10;i<=1000;i+=50){
-    //int numUsers = 10;  // You can change this as needed
-
-    // Create a WiFiSimulator object with the specified number of users
+    for(int i=1;i<=100;i++){
     WiFiSimulator simulator(i);
-
-    // Run the simulation and display the results
     simulator.runSimulation();
     }
     return 0;
